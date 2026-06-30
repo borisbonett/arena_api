f1a6a88fd97a33d6a92c60cbd75c39c5d05639a2
Boris Bonett <borisbonett@MacBook-Pro-de-Boris.local>
2026-06-29 23:51:22 -0500

    add implementation auth

--- app/controllers/api/v1/authentication_controller.rb
+++ app/controllers/api/v1/authentication_controller.rb
@@ -2,18 +2,29 @@ class Api::V1::AuthenticationController < ApplicationController
   skip_before_action :authorize_request, only: :login
 
   def login
-
-    # logger.info "--- MIS PARÁMETROS: #{params.inspect} ---"
-
     if params[:email].blank? || params[:password].blank?
       return render json: { error: 'El correo y la contraseña son obligatorios' }, status: :bad_request
     end
 
     @user = User.find_by_email(params[:email])
+    
     if @user&.authenticate(params[:password])
       token = JsonWebToken.encode(user_id: @user.id)
       time = Rails.env.development? ? 24.hours.from_now : 2.hours.from_now
-      render json: { token: token, exp: time.strftime("%Y-%m-%d %H:%M"), role: @user.role, username: @user.name }, status: :ok
+      
+      avatar_url = if @user.avatar.attached?
+        Rails.application.routes.url_helpers.rails_blob_path(@user.avatar, only_path: true)
+      else
+        nil
+      end
+
+      render json: { 
+        token: token, 
+        exp: time.strftime("%Y-%m-%d %H:%M"), 
+        role: @user.role, 
+        username: @user.name,
+        avatar_url: avatar_url # <--- Enviamos la ruta al frontend
+      }, status: :ok
     else
       render json: { error: 'Credenciales incorrectas' }, status: :unauthorized
     end
